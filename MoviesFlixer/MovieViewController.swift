b5327550d0801a9907c2feac63ec03aa6326ad82
Hector Aguirre <haguirre@mail.sfsu.edu>
2019-05-16 15:22:00 -0700

    Initial Commit

---
+++ MoviesFlixer/MovieViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MovieViewController.swift
+//  MoviesFlixer
+//
+//  Created by Pibe Aguirre on 5/16/19.
+//  Copyright © 2019 915235482. All rights reserved.
+//
+
+import UIKit
+
+class MovieViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Podfile
@@ -0,0 +1,21 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'MoviesFlixer' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for MoviesFlixer
+   pod "AlamofireImage"
+
+  target 'MoviesFlixerTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'MoviesFlixerUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
