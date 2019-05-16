e7a089950e35650ece03a4005278473ff81aab27
Hector Aguirre <haguirre@mail.sfsu.edu>
2019-05-16 15:32:11 -0700

    CSC 690 Project

---
+++ MoviesFlixer/MovieGridCell.swift
@@ -0,0 +1,16 @@
+//
+//  MovieGridCell.swift
+//  MoviesFlixer
+//
+//  Created by Pibe Aguirre on 5/16/19.
+//  Copyright © 2019 915235482. All rights reserved.
+//
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+}
